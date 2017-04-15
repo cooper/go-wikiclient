c01ed19169c4e8f78b7b196fdfcc50d13c593035
Mitchell Cooper <githubspam@rlygd.net>
2017-04-15 18:16:07 -0400

    more work on run transport

--- transport-run.go
+++ transport-run.go
@@ -5,23 +5,56 @@ package wikiclient
 // it runs the wikiserver as a subprocess of quiki.
 
 import (
-
+	"bufio"
+	"os/exec"
 )
 
 type RunTransport struct {
 	*jsonTransport
-	wikiserverPath string
+	wikifierPath string
+	configPath   string
+	cmd          *exec.Cmd
 }
 
 // create
-func NewRunTransport(path string) (*RunTransport, error) {
+func NewRunTransport(wikifierPath, configPath string) (*RunTransport, error) {
 	return &RunTransport{
 		createJson(),
-		path,
+		wikifierPath,
+		configPath,
+		nil,
 	}, nil
 }
 
 // connect
 func (tr *RunTransport) Connect() error {
-    return nil
+
+	// create readwriter for stdio
+	var reader bufio.Reader
+	var writer bufio.Writer
+	tr.reader = &reader
+	tr.writer = &writer
+	rw := bufio.NewReadWriter(&reader, &writer)
+
+	// create command
+	cmd := exec.Command("wikiserver", "--std", tr.configPath)
+	cmd.Dir = tr.wikifierPath
+	cmd.Stdout = rw
+	cmd.Stdin = rw
+	tr.cmd = cmd
+
+	// start the command
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	tr.connected = true
+	tr.startLoops()
+	return nil
+}
+
+func (tr *RunTransport) startLoops() {
+	// TODO: run continuously
+	go tr.cmd.Wait()
+	tr.jsonTransport.startLoops()
 }
